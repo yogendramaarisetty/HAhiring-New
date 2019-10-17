6ee88e26a8e6932b638f8480dff99cb1954792dc
yogendramaarisetty <yogendramaarisetty@gmail.com>
2019-10-17 18:02:13 +0530

    commt all

--- app/static/app/scripts/javascript.js
+++ app/static/app/scripts/javascript.js
@@ -1,3 +1,48 @@
+$(document).ready(function() {
+$
+var e = ace.edit("code_editor");
+e.getSession().setMode("ace/mode/java");
+var selected="";
+$("#q1_btn").click(function(){
+    $(selected).css("background-color","#FFFFFF");
+    $(selected).css("color","black");
+    $("#q1_btn").css("background-color","green");
+    $("#q1_btn").css("color","white");
+    selected = "#q1_btn";
+})
+
+$("#q2_btn").click(function(){
+    $(selected).css("background-color","#FFFFFF");
+    $(selected).css("color","black");
+    $("#q2_btn").css("background-color","green");
+    $("#q2_btn").css("color","white");
+    selected = "#q2_btn";
+})
+$("#q3_btn").click(function(){
+    $(selected).css("background-color","#FFFFFF");
+    $(selected).css("color","black");
+    $("#q3_btn").css("background-color","green");
+    $("#q3_btn").css("color","white");
+    selected = "#q3_btn";
+})
+$("#q4_btn").click(function(){
+    $(selected).css("background-color","#FFFFFF");
+    $(selected).css("color","black");
+    $("#q4_btn").css("background-color","green");
+    $("#q4_btn").css("color","white");
+    selected = "#q4_btn";
+})
+$("#q5_btn").click(function(){
+    $(selected).css("background-color","#FFFFFF");
+    $(selected).css("color","black");
+    $("#q5_btn").css("background-color","green");
+    $("#q5_btn").css("color","white");
+    selected = "#q5_btn";
+})
+
+})
+
+
 var date=new Date()
 $(document).ready( function(){
     $(".testdate").html(date.toDateString());
